584292cc0d5228259e230bd78e387a0c4e665918
alma-siwon <alma-siwon@github.com>
2014-10-25 16:31:46 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Oct 24 17:35:53 KST 2014"
+#define LINUX_COMPILE_BY "alma"
+#define LINUX_COMPILE_HOST "alma-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
